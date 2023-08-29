f55d07c590e05e5f538cae29a2db0ed0f09d1e33
jean.bellio <jean_bellio@sicredi.com.br>
2023-08-29 10:00:58 -0300

    productExceptSelf correction

--- src/main/java/org/example/ProductExceptSelf.java
+++ src/main/java/org/example/ProductExceptSelf.java
@@ -3,7 +3,7 @@ package org.example;
 public class ProductExceptSelf {
 
     public static void main(String[] args) {
-        int[] nums = {-1,1,0,-3,3};
+        int[] nums = {1,2,3,4};
         int[] products = productExceptSelf(nums);
         for (int product : products) {
             System.out.println(product);
@@ -12,15 +12,19 @@ public class ProductExceptSelf {
 
     private static int[] productExceptSelf(int[] nums) {
         int[] products = new int[nums.length];
+        int aux = 1;
         for (int i = 0; i < nums.length; i++) {
-            int aux = 1;
-            for (int j = 0; j < nums.length; j++) {
-                if(i != j) {
-                    aux = aux * nums[j];
-                }
-            }
             products[i] = aux;
+            aux *= nums[i];
+        }
+
+        aux = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            products[i] *= aux;
+            aux *= nums[i];
         }
         return products;
     }
+
 }
+
